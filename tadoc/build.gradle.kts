faa65af33193f3e80e87614aa2c84d8ec9e52483
Alexander Weigl <weigl@kit.edu>
2022-01-11 10:01:46 +0100

    repair gradle

---
+++ tadoc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+    implementation("com.atlassian.commonmark:commonmark:0.14.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-gfm-tables:0.14.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-autolink:0.14.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-gfm-strikethrough:0.14.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-ins:0.14.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-heading-anchor:0.14.0")
+//    compile group: 'org.eclipse.jgit', name: 'org.eclipse.jgit', version: '5.7.0.202003110725-r'
+}
